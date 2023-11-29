43b2efe2b578821efb4cbbec006fc202f69dedbd
mrodavell <jomar.llevado@ustp.edu.ph>
2023-11-29 16:19:07 +0800

    completed sign up and login

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,9 +1,50 @@
-import { View } from "react-native";
+import { View, ToastAndroid } from "react-native";
 import React from "react";
 import { Button, Text, TextInput } from "react-native-paper";
+import fetchServices from "../services/fetchServices";
 
 export default function LoginForm({ navigation }) {
   const [showPass, setShowPass] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [errors, setErrors] = React.useState({});
+  const [loading, setLoading] = React.useState(false);
+
+  const showToast = (message = "Something wen't wrong") => {
+    ToastAndroid.show(message, 3000);
+  };
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      if (email === "") {
+        setErrors({ email: true });
+        return false;
+      }
+
+      if (password === "") {
+        setErrors({ password: true });
+        return false;
+      }
+
+      const url = "http://172.20.9.250/api/v1/login";
+      const data = {
+        email,
+        password,
+      };
+      const result = await fetchServices.postData(url, data);  
+      console.debug(result);     
+      if (result.message != null) {
+        showToast(result?.message);
+      } else {
+        navigation.navigate("Home");
+      }
+    } catch (e) {
+      console.debug(e.toString());
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View styles={{ flex: 1 }}>
       <Text variant="displayMedium">Login</Text>
@@ -12,7 +53,9 @@ export default function LoginForm({ navigation }) {
         placeholder="Email"
         label="Email"
         style={{ marginTop: 10 }}
-        error={true}
+        value={email}
+        onChangeText={setEmail}
+        error={errors?.email}
       />
       <TextInput
         mode="outlined"
@@ -26,11 +69,22 @@ export default function LoginForm({ navigation }) {
           />
         }
         style={{ marginTop: 10 }}
+        value={password}
+        onChangeText={setPassword}
+        error={errors?.password}
       />
-      <Button icon="login" mode="contained" style={{ marginTop: 10 }}>
+      <Button
+        loading={loading}
+        disabled={loading}
+        onPress={handleLogin}
+        icon="login"
+        mode="contained"
+        style={{ marginTop: 10 }}
+      >
         Login
       </Button>
       <Button
+        disabled={loading}
         onPress={() => navigation.navigate("Register")}
         icon="account-plus"
         mode="contained"
